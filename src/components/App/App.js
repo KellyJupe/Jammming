234c5a9930e8a3a05965b7f2927bc51faa483243
KellyJupe <37583656+KellyJupe@users.noreply.github.com>
2018-05-17 22:37:33 -0400

    5/17 pm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,26 +10,31 @@ constructor(props) {
   super(props);
     this.state = {
       searchResults: [
-        {name: "name", artist: "artist", album: "album", id: "id"},
-        {name: "name", artist: "artist", album: "album", id: "id"},
-        {name: "name", artist: "artist", album: "album", id: "id"},
-      ]
+        {name: "", artist: "", album: "", id: ""},
+        {name: "", artist: "", album: "", id: ""},
+        {name: "", artist: "", album: "", id: ""},
+      ],
+      playlistName: 'Déchirant',
+      playlistTracks: [
+        {name: 'Say Something', artist: 'Great Big World', album: 'Sad', id: 'ss1'},
+        {name: "How Far", artist: "Martina McBride", album: "Goodbye", id: "hf1"},
+        {name: "All at Once", artist: "Whitney Houston", album: "Oldies", id: "ao1"},
+      ],
+
     }
 };
   render() {
     return (
-      <div>
+    <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-      <h2>{this.props.track.name}</h2>
     <div className="App-playlist">
+      <h2>playlistName: {this.state.playlistName}</h2>
+      <h2>playlistTracks: this.state.playlistTracks</h2>
 
     </div>
-  </div>
-</div>
+    </div>
     );
   }
 }
 
 export default App;
-ReactDOM.render(<h2 />, document.getElementById('track'));
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -12,16 +12,15 @@ class Track extends Component {
       return '-';
       }
     }
-
-  render() {
+    render() {
     return (
-<div className="Track">
-  <div className="Track-information">
-    <h3></h3>
-    <p> | </p>
-  </div>
-  <a className="Track-action">{this.renderAction}</a>
-</div>
+    <div className="Track">
+      <div className="Track-information">
+        <h3>{this.props.track.name}</h3>
+        <p>{this.props.track.artist} | {this.props.track.album}</p>
+      </div>
+      {this.renderAction()}
+    </div>
     );
   }
 }
